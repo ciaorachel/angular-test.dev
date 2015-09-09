af7d959b485c328da477f738a4b0d02116a884f7
ciaorachel <ciaorachel@gmail.com>
2015-09-08 23:43:50 -0500

    completed exercise on models in angular, then updated by adding a controller

--- public/js/modelsTest.js
+++ public/js/modelsTest.js
@@ -2,4 +2,15 @@
 	"use strict";
 
 	var app = angular.module("modelsTest", []);
+
+	app.controller("UsersController", function() {
+		this.newUser = {};
+		this.users = [];
+
+		this.addUser = function() {
+			this.users.push(this.newUser);
+			this.newUser = {};
+		};
+	});
+
 }) ();
